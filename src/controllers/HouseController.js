efcae74298c580fa0d162876334ff393a0442cce
thiagofm2 <thiago.fm012@gmail.com>
2024-01-06 09:49:50 -0300

    .

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,5 +1,6 @@
 import House from "../models/House.js";
-import User from '../models/User.js'
+import User from '../models/User.js';
+import * as Yup from 'yup'
 
 class HouseController{
 
@@ -11,9 +12,20 @@ class HouseController{
     }
 
     async store(req, res){
+        const schema = Yup.object().shape({
+            description: Yup.string().required(),
+            price: Yup.number().required(),
+            location: Yup.string().required(),
+            status: Yup.boolean().required(),
+        });
+
         const { filename } = req.file;
         const { description, price, location, status } = req.body;
         const { user_id } = req.headers;
+
+        if(!(await schema.isValid(req.body))){
+            return res.status(400).json({error: "Erro na validação. Verifique os campos."})
+        }
 
         const house = await House.create({
             user: user_id,
@@ -27,7 +39,20 @@ class HouseController{
     }
 
     async update(req, res){
-        const { filename } = req.file;
+        const schema = Yup.object().shape({
+            description: Yup.string().required(),
+            price: Yup.number().required(),
+            location: Yup.string().required(),
+            status: Yup.boolean().required(),
+        });
+
+        let photos;
+
+        if(req.file){
+            photos = req.file.filename
+        }
+
+        // const { filename } = req.file;
         const { description, price, location, status } = req.body;
         const { id } = req.params;
         const { user_id } = req.headers;
@@ -35,15 +60,17 @@ class HouseController{
         const user = await User.findById(user_id);
         const house = await House.findById(id);
 
+        if(!(await schema.isValid(req.body))){
+            return res.status(400).json({error: "Erro na validação. Verifique os campos."})
+        }
 
-        console.log(house.status)
         if(String(user._id) !== String(house.user)){
             return res.status(401).json({ error: 'Não autorizado.'});
         }
 
         await House.updateOne({ _id: id }, {
             user: user_id,
-            thumbnail: filename,
+            thumbnail: photos,
             description,
             price,
             location,
